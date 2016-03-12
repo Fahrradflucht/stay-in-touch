a692940fd168e4153832b95c65665dcd863d2e41
Fahrradflucht <mail@mathiswiehl.de>
2016-03-12 17:15:29 +0100

    Show contacts on main page

--- client/app/contacts/contacts.controller.js
+++ client/app/contacts/contacts.controller.js
@@ -2,8 +2,32 @@
 (function(){
 
 class ContactsComponent {
-  constructor() {
-    this.message = 'Hello';
+  constructor($http, $scope, socket) {
+    this.$http = $http;
+    this.socket = socket;
+    this.contacts = [];
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('contact');
+    });
+  }
+
+  $onInit() {
+    this.$http.get('/api/contacts').then(response => {
+      this.contacts = response.data;
+      this.socket.syncUpdates('contact', this.contacts);
+    });
+  }
+
+  addContact() {
+    if (this.newContact) {
+      this.$http.post('/api/contacts', { name: this.newContact });
+      this.newContact = '';
+    }
+  }
+
+  deleteContact(contact) {
+    this.$http.delete('/api/contacts/' + contact._id);
   }
 }
 
