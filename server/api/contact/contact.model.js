74ba29ce1fff04860eee5278d2c71e17ca78ea89
Fahrradflucht <mail@mathiswiehl.de>
2016-03-13 17:25:42 +0100

    Change contact model
    Add:
      * Contact Frequencies
      * Meetings

--- server/api/contact/contact.model.js
+++ server/api/contact/contact.model.js
@@ -4,6 +4,12 @@ import mongoose from 'mongoose';
 
 var ContactSchema = new mongoose.Schema({
   name: String,
+  idealContactFrequency: Number,
+  minimumContactFrequency: Number,
+  meetings: [{
+    note: String,
+    date: Date,
+  }],
   user: {
     type: mongoose.Schema.ObjectId,
     ref: 'User'
