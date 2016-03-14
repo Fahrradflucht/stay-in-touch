bf134ca5e9cc13c1aca84bd59dcf546376e674bf
Fahrradflucht <mail@mathiswiehl.de>
2016-03-14 14:19:08 +0100

    Redo frontend-design to incorporate status

--- client/app/contacts/contacts.controller.js
+++ client/app/contacts/contacts.controller.js
@@ -1,40 +1,54 @@
 'use strict';
 (function(){
 
-class ContactsComponent {
-  constructor($http, $scope, socket) {
-    this.$http = $http;
-    this.socket = socket;
-    this.contacts = [];
+  class ContactsComponent {
+    constructor($http, $scope, socket) {
+      this.$http = $http;
+      this.socket = socket;
+      this.contacts = [];
 
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('contact');
-    });
-  }
+      $scope.$on('$destroy', function() {
+        socket.unsyncUpdates('contact');
+      });
+    }
 
-  $onInit() {
-    this.$http.get('/api/contacts').then(response => {
-      this.contacts = response.data;
-      this.socket.syncUpdates('contact', this.contacts);
-    });
-  }
+    $onInit() {
+      this.$http.get('/api/contacts').then(response => {
+        this.contacts = response.data;
+        this.socket.syncUpdates('contact', this.contacts);
+      });
+    }
 
-  addContact() {
-    if (this.newContact) {
-      this.$http.post('/api/contacts', { name: this.newContact });
-      this.newContact = '';
+    addContact() {
+      if (this.newContact) {
+        this.$http.post('/api/contacts', {
+          name: this.newContact.name,
+          idealContactFrequency: this.newContact.idealContactFrequency,
+          minimumContactFrequency: this.newContact.minimumContactFrequency
+        });
+        this.newContact = {};
+      }
+    }
+
+    deleteContact(contact) {
+      this.$http.delete('/api/contacts/' + contact._id);
+    }
+
+    addInteraction(contact) {
+      if (this.newInteraction) {
+        this.$http.post('/api/contacts/' + contact._id + '/interactions', {
+          note: this.newInteraction.note
+        });
+        this.newInteraction = {};
+      }
     }
   }
 
-  deleteContact(contact) {
-    this.$http.delete('/api/contacts/' + contact._id);
-  }
-}
 
 angular.module('stayInTouchApp')
-  .component('contacts', {
-    templateUrl: 'app/contacts/contacts.html',
-    controller: ContactsComponent
-  });
+.component('contacts', {
+  templateUrl: 'app/contacts/contacts.html',
+  controller: ContactsComponent
+});
 
 })();
